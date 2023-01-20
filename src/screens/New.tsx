73b59775d87c2a210a6798d6f6d4b971377be473
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-20 17:09:04 -0300

    criando o botão de confirmar

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { View, ScrollView, Text, TextInput } from "react-native";
+import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { Feather } from '@expo/vector-icons';
+import colors from "tailwindcss/colors";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
@@ -18,7 +20,7 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16" >
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 }}>
         <BackButton />
         <Text className="mt-6 text-white font-extrabold text-3xl">
           Criar hábito
@@ -28,6 +30,8 @@ export function New() {
         </Text>
         <TextInput 
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
         />
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
@@ -42,7 +46,19 @@ export function New() {
             />
           ))
         }
-        
+        <TouchableOpacity 
+          activeOpacity={0.7}
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+        >
+          <Feather 
+            name='check'
+            size={20}
+            color={colors.white}
+          />
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   )
