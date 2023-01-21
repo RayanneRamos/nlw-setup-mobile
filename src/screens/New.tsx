e421962038f66f45c22a812cec05e991d4cdc2f3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-20 23:01:57 -0300

    obtendo informações do formulário e criando um novo hábito

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,20 +1,43 @@
 import { useState } from "react";
-import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 import { Feather } from '@expo/vector-icons';
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
   const [ weekDays, setWeekDays ] = useState<number[]>([]);
+  const [ title, setTitle ] = useState('');
 
   function handleToggleWeekDay(weekDayIndex: number) {
     if(weekDays.includes(weekDayIndex)) {
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex]);
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if(!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.');
+      }
+
+      await api.post('/habits', {
+        title,
+        weekDays,
+      });
+
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo hábito', 'Seu hábito foi criado com sucesso!');
+    } catch(error) {
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito');
+      console.log(error);
     }
   }
 
@@ -32,6 +55,8 @@ export function New() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
@@ -49,6 +74,7 @@ export function New() {
         <TouchableOpacity 
           activeOpacity={0.7}
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          onPress={handleCreateNewHabit}
         >
           <Feather 
             name='check'
