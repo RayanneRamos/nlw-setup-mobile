d3dfd76fd909e4430219669d974ad0f379757995
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-28 22:19:11 -0300

    criando as notificações

--- App.tsx
+++ App.tsx
@@ -1,29 +1,62 @@
-import { StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
-import { Loading } from './src/components/Loading';
-import './src/lib/dayjs';
-import { Routes } from './src/routes';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
+import "./src/lib/dayjs";
+import { Routes } from "./src/routes";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_800ExtraBold,
   });
 
-  if(!fontsLoaded) {
-    return (
-      <Loading />
-    );
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Rayanne!",
+        body: "Você praticou seus hábitos hoje?",
+      },
+      trigger,
+    });
   }
 
+  async function getScheduleNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedules);
+  }
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
 
   return (
     <>
       <Routes />
-      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </>
   );
 }
-
